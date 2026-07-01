fd38ff53cac7dd6a827ae44f4e68babf578fc4d8
johnathan <notbuffalo09@gmail.com>
2026-07-01 13:30:44 -0400

    feat: vite + electron template

--- electron-app/main.js
+++
@@ -1,9 +0,0 @@
-const { app, BrowserWindow } = require("electron")
-
-function createWindow() {
-    const win = new BrowserWindow({width: 300, height: 300});
-    win.loadFile('index.html')
-}
-
-app.whenReady().then(createWindow)
-
